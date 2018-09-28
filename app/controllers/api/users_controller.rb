54a97da1ca9b5d827f81e0942bfabb0e6f241a50
svenlarson1098 <svenlarson1098@gmail.com>
2018-09-27 19:48:02 -0700

    Checkpoint 2 Complete

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,9 @@
-class API::UsersController < ApiController
-  
+class Api::UsersController < ApiController
   before_action :authenticated?
-
+  
   def index
-    users = User.all  
+    users = User.all
+    
     render json: users, each_serializer: UserSerializer
   end
-
 end
